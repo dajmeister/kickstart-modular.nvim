c37d679ee5ba99fae788b9a40ac0be44a240ff82
John Childs <dajmeister@gmail.com>
2025-05-26 14:41:57 -0400

    customizing nvim config

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -28,7 +28,7 @@ require('lazy').setup({
 
   require 'kickstart.plugins.blink-cmp',
 
-  require 'kickstart.plugins.tokyonight',
+  -- require 'kickstart/plugins/tokyonight',
 
   require 'kickstart.plugins.todo-comments',
 
@@ -47,7 +47,7 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
 
@@ -55,7 +55,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
