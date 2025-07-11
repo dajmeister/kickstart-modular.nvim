e9ab06a17c38d696a8c9e50e71f19b8c4fc50fd8
John Childs <dajmeister@gmail.com>
2025-07-11 09:29:36 -0400

    indentation updates

--- lua/options.lua
+++ lua/options.lua
@@ -62,26 +62,21 @@ vim.o.inccommand = 'split'
 vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
+vim.o.scrolloff = 8
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
-vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
-
-vim.o.smartindent = true
 
 vim.o.wrap = false
 
 vim.o.termguicolors = true
 
-vim.o.scrolloff = 8
-vim.opt.signcolumn = 'yes'
 vim.o.colorcolumn = '80'
 
 -- vim: ts=2 sts=2 sw=2 et
