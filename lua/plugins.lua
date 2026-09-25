92f9f0054b9c02c3654cc93066ad11007766119e
John Childs <dajmeister@gmail.com>
2026-09-25 15:55:46 -0400

    refactor: drop nvim-lint and markdownlint
    markdownlint was the only configured linter and was never installed, so every BufEnter on a markdown buffer raised ENOENT.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -23,7 +23,6 @@ require 'kickstart.plugins.treesitter'
 --
 -- require 'kickstart.plugins.debug'
 require 'kickstart.plugins.indent_line'
-require 'kickstart.plugins.lint'
 require 'kickstart.plugins.autopairs'
 -- require 'kickstart.plugins.neo-tree'
 
